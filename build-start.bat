d4774f492cc96b4195dde3f9d1c2745f75f2659c
j-mye <153039331+j-mye@users.noreply.github.com>
2025-10-17 12:10:01 -0500

    Clean up tracked files according to .gitignore

--- build-start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start "" "C:\Users\myersjr\OneDrive - Milwaukee School of Engineering\Documents\Courses\JS1\cpp\planets\build.bat"
-timeout /t 2 /nobreak > nul
-start "" "C:\Users\myersjr\OneDrive - Milwaukee School of Engineering\Documents\Courses\JS1\cpp\planets\build\Debug\PlanetsProject.exe"
--- start.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "C:\Users\myersjr\OneDrive - Milwaukee School of Engineering\Documents\Courses\JS1\cpp\planets\build\Debug\PlanetsProject.exe"
